b453bebc5706e9d2b31cbb78722f0be8a02b838e
HenryFernando <henferfon@gmail.com>
2021-04-15 20:21:16 -0500

    Ejemplo vista

---
+++ resources/views/casa.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.arrenline')
+@section('content')
+
+<section>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        <br>
+         Molestias dignissimos, magni incidunt saepe inventore facere
+        <br>
+          qui delectus? Totam culpa earum dolorum consectetur quo, quam unde,
+        <br>
+          dicta deserunt distinctio eveniet officiis.
+    </p>
+</section>
+
+@endsection
